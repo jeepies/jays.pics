11153c32137e764d416a76a779500192bb1a8813
jay <davisjayalan@gmail.com>
2024-09-28 01:15:05 +0100

    added basic image webpage

--- app/routes/i.$id.tsx
+++ app/routes/i.$id.tsx
@@ -5,18 +5,33 @@ import { getSession, getUserBySession } from "~/services/session.server";
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
   const image = await prisma.image.findFirst({ where: { id: params.id } });
-
-  // @TODO get image url, if exists on CloudFront. if not, get from S3 and upload to cloudfront for a limited duration
+  const uploader = await prisma.user.findFirst({
+    where: { id: image!.uploader_id },
+  });
 
   const session = await getSession(request.headers.get("Cookie"));
   const user = (await getUserBySession(session)) ?? {};
-  return { image, user };
+
+  return { data: { image: image, uploader: uploader }, user };
 }
 
 export default function Image() {
-  const { image, user } = useLoaderData<typeof loader>();
+  const { data, user } = useLoaderData<typeof loader>();
 
-  return <>
-  {image !== null ? <>image found {user !== null ? <>comment box with comment ability</>:<>comment box, but no comment ability :(</>}</> : <>no image</>}
-  </>
+  return (
+    <>
+      {data.image !== null ? (
+        <>
+          image found
+          {user !== null ? (
+            <>comment box with comment ability</>
+          ) : (
+            <>comment box, but no comment ability :(</>
+          )}
+        </>
+      ) : (
+        <>no image</>
+      )}
+    </>
+  );
 }
