e9978a57795a0a9724e611e0c51722c46a56381a
jay <davisjayalan@gmail.com>
2024-09-28 15:09:13 +0100

    added get-templates api route

---
+++ app/routes/api.get-templates.tsx
@@ -0,0 +1,21 @@
+import { json } from "@remix-run/node";
+
+export async function loader() {
+  return json({
+    success: true,
+    data: {
+      "image.name": "Gets the display name of the image",
+      "image.size_bytes": "Gets the images size in bytes",
+      "image.size": "Gets the human-readable image size",
+      "image.created_at": "Gets the date the image was created on",
+
+      "uploader.name": "Gets the uploader's username",
+      "uploader.storage_used_bytes": "Gets the uploader's storage used",
+      "uploader.storage_used": "Gets the human-readable uploader's storage used",
+      "uploader.total_storage_bytes": "Gets the uploader's total storage capacity",
+      "uploader.total_storage": "Gets the human-readable uploader's total storage capacity",
+
+      "random": "Gets a random number (0-100)",
+    },
+  });
+}
