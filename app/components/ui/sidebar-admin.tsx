d320d7fd3311678f8f840e47a95b5a9f2551a402
jay <davisjayalan@gmail.com>
2024-10-01 17:01:39 +0100

    added domains admin page

--- app/components/ui/sidebar-admin.tsx
+++ app/components/ui/sidebar-admin.tsx
@@ -23,6 +23,7 @@ import {
   Images,
   Users,
   Logs,
+  Globe2,
 } from "lucide-react";
 import { ThemeToggle } from "./themetoggle";
 import { Separator } from "./separator";
@@ -74,6 +75,16 @@ export function SidebarAdmin({ className, user }: SidebarProps) {
               <Link to="/admin/images">
                 <Images className="mr-2 h-4 w-4" />
                 Images
+              </Link>
+            </Button>
+            <Button
+              asChild
+              variant="ghost"
+              className="w-full justify-start text-gray-900 dark:text-gray-100"
+            >
+              <Link to="/admin/domains">
+                <Globe2 className="mr-2 h-4 w-4" />
+                Domains
               </Link>
             </Button>
             <Button
---
+++ app/routes/_admin.admin.domains.tsx
@@ -0,0 +1,91 @@
+import { LoaderFunctionArgs, redirect } from "@remix-run/node";
+import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { Input } from "~/components/ui/input";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "~/components/ui/table";
+import { useAdminLoader } from "./_admin";
+import { prisma } from "~/services/database.server";
+import { useLoaderData } from "@remix-run/react";
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const count = await prisma.uRL.count();
+  const urls = await prisma.uRL.findMany({
+    select: {
+        url: true,
+        public: true,
+        progress: true,
+        zone_id: true,
+        donator: {
+            select: {
+                id: true,
+                username: true,
+            },
+        },
+        created_at: true,
+    }
+  });
+
+  return { count, urls };
+}
+
+export default function Users() {
+  const me = useAdminLoader();
+  const { count, urls } = useLoaderData<typeof loader>();
+
+  return (
+    <>
+      {" "}
+      <Card className="mt-4">
+        <CardHeader>
+          <CardTitle>Logs</CardTitle>
+          <CardDescription>There are {count} domains</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="max-w-96">Domain</TableHead>
+                <TableHead className="max-w-96">Public</TableHead>
+                <TableHead className="max-w-96">Status</TableHead>
+                <TableHead className="max-w-96">Zone</TableHead>
+                <TableHead className="max-w-96">Donator</TableHead>
+                <TableHead className="text-right">Date of Creation</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {urls.map((url) => {
+                return (
+                  <TableRow>
+                    <TableCell className="font-medium">{url.url}</TableCell>
+                    <TableCell>{url.public ? "Yes" : "No"}</TableCell>
+                    <TableCell>{url.progress}</TableCell>
+                    <TableCell>{url.zone_id}</TableCell>
+                    <TableCell><a href={`/admin/profile/${url.donator!.id}`}>{url.donator!.username}</a></TableCell>
+                    <TableCell className="text-right">
+                      {new Date(url.created_at).toLocaleDateString()} @{" "}
+                      {new Date(url.created_at).toLocaleTimeString()}
+                    </TableCell>
+                  </TableRow>
+                );
+              })}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+    </>
+  );
+}
