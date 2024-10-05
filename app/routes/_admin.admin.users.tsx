7d25d1859c9335ad49a05867f2af405bcfc15850
jay <davisjayalan@gmail.com>
2024-10-05 23:19:39 +0100

    changed image_uploaded column

--- app/routes/_admin.admin.users.tsx
+++ app/routes/_admin.admin.users.tsx
@@ -68,7 +68,7 @@ export default function Users() {
                     <TableCell className="font-medium">
                       <a href={`/admin/profile/${user.id}`}>{user.username}</a>
                     </TableCell>
-                    <TableCell>{user.images.length} ({prettyBytes(user.space_used)})</TableCell>
+                    <TableCell>{user.images.filter((image) => image.deleted_at === null).length} ({prettyBytes(user.space_used)}, w/ deleted: {user.images.length})</TableCell>
                     <TableCell>{user.donated_urls.length}</TableCell>
                     <TableCell>{user.is_admin ? "Yes" : "No"}</TableCell>
                     <TableCell className="text-right">
