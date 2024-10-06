a5f19bfdb23e263f17f6d930b62cb7e8e03e5109
jay <davisjayalan@gmail.com>
2024-10-06 23:05:18 +0100

    rewrote add flow

--- app/routes/_app.dashboard.domain.add.tsx
+++ app/routes/_app.dashboard.domain.add.tsx
@@ -1,32 +1,63 @@
 import { Progress } from "@prisma/client";
-import {
-  ActionFunctionArgs,
-  LoaderFunctionArgs,
-  redirect,
-} from "@remix-run/node";
+import { Label } from "@radix-ui/react-label";
+import { ActionFunctionArgs, LoaderFunctionArgs, redirect } from "@remix-run/node";
 import { Form, useActionData, useLoaderData } from "@remix-run/react";
 import { CloudflareError } from "cloudflare";
 import { z } from "zod";
 import { Button } from "~/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
 import { Input } from "~/components/ui/input";
-import { Label } from "~/components/ui/label";
-import { createZone, getNameServers } from "~/services/cloudflare.server";
+import { createZone } from "~/services/cloudflare.server";
 import { prisma } from "~/services/database.server";
 import { getSession, getUserBySession } from "~/services/session.server";
 
+const domainSchema = z.object({
+  domain: z
+    .string()
+    .regex(/[a-z-]+\.[a-z]+/i, "This domain is invalid.")
+    .optional(),
+});
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const url = new URL(request.url);
+  const paramEntries = Object.fromEntries(url.searchParams.entries());
+  const result = domainSchema.safeParse(paramEntries);
+
+  if (!result.success) {
+    const error = result.error.flatten();
+    return {
+      paramEntries,
+      formErrors: error.formErrors,
+      fieldErrors: error.fieldErrors,
+    };
+  }
+
+  let domain = null;
+  if (result.data.domain) {
+    domain = await prisma.uRL.findFirst({
+      where: {
+        url: result.data.domain,
+      },
+    });
+  }
+
+  return domain;
+}
+
 export default function AddDomain() {
-  const { domain, data } = useLoaderData<typeof loader>();
+  const data = useLoaderData<typeof loader>();
   const actionData = useActionData<typeof action>();
 
-  return (
-    <div className="container mx-auto px-4 py-8">
-      <Card>
-        <CardHeader>
-          <CardTitle>What domain would you like to link?</CardTitle>
-        </CardHeader>
-        <CardContent>
-          {!domain ? (
+  console.log(data);
+  console.log(actionData);
+
+  if (data === null) {
+    // This domain doesn't exist in our database
+    return (
+      <div className="container mx-auto px-4 py-8">
+        <Card>
+          <CardHeader>What domain would you like to link?</CardHeader>
+          <CardContent>
             <Form method="POST">
               <Input
                 name="action"
@@ -34,80 +65,107 @@ export default function AddDomain() {
                 readOnly
                 className="hidden"
               />
-              <Input id="domain" placeholder="domain.com" name="domain" />
+              <Input name="domain" placeholder="domain.com" />
               <Button className="mt-2 w-full">Start</Button>
-            </Form>
-          ) : (
-            <>
-              <Input readOnly defaultValue={domain} name="domain" />
-              <Button className="mt-2 w-full" disabled>
-                Start
-              </Button>
-            </>
-          )}
-        </CardContent>
-      </Card>
-
-      {!domain ? (
-        <></>
-      ) : (
-        <Card className="mt-2">
-          <CardHeader>
-            <CardTitle>Change Nameservers</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <Label>Please update your nameservers to point too</Label>
-            <Input
-              className="mt-2"
-              readOnly
-              defaultValue={data!.nameServers[0]}
-            />
-            <Input
-              className="mt-2"
-              readOnly
-              defaultValue={data!.nameServers[1]}
-            />
-            <Form method="POST">
-              <Input
-                name="domain"
-                defaultValue={domain}
-                readOnly
-                className="hidden"
-              />
-              <Input
-                name="action"
-                value="updated_nameservers"
-                readOnly
-                className="hidden"
-              />
-              <Button className="mt-2 w-full">Done</Button>
             </Form>
           </CardContent>
         </Card>
-      )}
-    </div>
-  );
-}
-
-export async function loader({ request }: LoaderFunctionArgs) {
-  const url = new URL(request.url);
-  const paramEntries = Object.fromEntries(url.searchParams.entries());
-
-  let data = { nameServers: ["", ""] };
-  if (paramEntries.zid) {
-    Object.assign(data, {
-      nameServers: await getNameServers(paramEntries.zid),
-    });
+      </div>
+    );
   }
 
-  return { domain: paramEntries.domain, data: data };
+  switch (data.progress) {
+    case Progress.INPUT:
+      return (
+        <div className="container mx-auto px-4 py-8">
+          <Card>
+            <CardHeader>              <CardTitle>What domain would you like to link?</CardTitle></CardHeader>
+            <CardContent>
+              <Input name="domain" defaultValue={data.url} readOnly />
+              <Button className="mt-2 w-full" disabled>
+                Start
+              </Button>
+            </CardContent>
+          </Card>
+          <Card className="mt-2">
+            <CardHeader>
+              <CardTitle>Change Nameservers</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <Label>Please update your nameservers to point too</Label>
+              <Input
+                className="mt-2"
+                readOnly
+                defaultValue={data!.nameservers[0]}
+              />
+              <Input
+                className="mt-2"
+                readOnly
+                defaultValue={data!.nameservers[1]}
+              />
+              <Form method="POST">
+                <Input
+                  name="domain"
+                  defaultValue={data!.url}
+                  readOnly
+                  className="hidden"
+                />
+                <Input
+                  name="action"
+                  value="updated_nameservers"
+                  readOnly
+                  className="hidden"
+                />
+                <Button className="mt-2 w-full">Done</Button>
+              </Form>
+            </CardContent>
+          </Card>
+        </div>
+      );
+    case Progress.WAITING:
+      return (
+        <div className="container mx-auto px-4 py-8">
+          <Card>
+            <CardHeader>              <CardTitle>What domain would you like to link?</CardTitle></CardHeader>
+            <CardContent>
+              <Input name="domain" defaultValue={data.url} readOnly />
+              <Button className="mt-2 w-full" disabled>
+                Start
+              </Button>
+            </CardContent>
+          </Card>
+          <Card className="mt-2">
+            <CardHeader>
+              <CardTitle>Change Nameservers</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <Label>Please update your nameservers to point too</Label>
+              <Input
+                className="mt-2"
+                readOnly
+                defaultValue={data!.nameServers[0]}
+              />
+              <Input
+                className="mt-2"
+                readOnly
+                defaultValue={data!.nameServers[1]}
+              />
+              <Form method="POST">
+                <Input
+                  name="action"
+                  value="updated_nameservers"
+                  readOnly
+                  className="hidden"
+                  disabled
+                />
+                <Button className="mt-2 w-full disabled">Waiting...</Button>
+              </Form>
+            </CardContent>
+          </Card>
+        </div>
+      )
+  }
 }
-
-const domainSchema = z.object({
-  domain: z
-    .string({ required_error: "This field is required" })
-    .regex(/[a-z-]+\.[a-z]+/i, "Please enter a valid domain"),
-});
 
 export async function action({ request }: ActionFunctionArgs) {
   const formData = await request.formData();
@@ -123,105 +181,50 @@ export async function action({ request }: ActionFunctionArgs) {
     };
   }
 
-  const requestType = formData.get("action");
+  const requestAction = formData.get("action");
 
-  if (requestType === "set_domain") {
-    const domain = await prisma.uRL.findFirst({
-      where: { url: result.data.domain },
+  const user = await getUserBySession(
+    await getSession(request.headers.get("Cookie"))
+  );
+
+  if (requestAction === "set_domain") {
+    // assume the worst
+    const domainCheck = await prisma.uRL.count({
+      where: { url: result.data?.domain },
     });
+    if (domainCheck !== 0) return null;
 
-    if (domain !== null) {
-      return {
-        payload,
-        formErrors: [],
-        fieldErrors: {
-          domain: "This domain already exists. Please refresh the page",
-        },
-      };
-    }
+    // assume the domain is brand new
 
-    const check = await prisma.uRL.count({
-      where: { url: result.data.domain },
-    });
-    if (check === 0) {
-      let zone;
-      try {
-        zone = await createZone(result.data.domain);
-      } catch (err: any) {
-        if (err instanceof CloudflareError) {
-          const e = JSON.parse(err.message.slice(4, err.message.length));
-          if (!e.errors[0].message.includes("already exists")) {
-            await prisma.log.create({
-              data: {
-                message: e.errors[0].message,
-              },
-            });
-            return {
-              payload,
-              formErrors: [
-                "An unknown error has occured. This has been logged.",
-              ],
-              fieldErrors: {
-                domain: null,
-              },
-            };
-          }
+    let zone;
+    try {
+      zone = await createZone(result.data.domain!);
+    } catch (err: any) {
+      if (err instanceof CloudflareError) {
+        const e = JSON.parse(err.message.slice(4, err.message.length));
+        if (!e.errors[0].message.includes("already exists")) {
+          await prisma.log.create({
+            data: {
+              message: e.errors[0].message,
+            },
+          });
         }
       }
-
-      const user = await getUserBySession(
-        await getSession(request.headers.get("Cookie"))
-      );
-
-      if (!zone) {
-        await prisma.log.create({
-          data: {
-            message: "Attempted to link domain to non-existant zone.",
-          },
-        });
-        return {
-          payload,
-          formErrors: ["An unknown error has occured. This has been logged."],
-          fieldErrors: {
-            domain: null,
-          },
-        };
-      }
-
-      await prisma.uRL.create({
-        data: {
-          donator_id: user?.id,
-          url: result.data.domain,
-          public: false,
-          connected: false,
-          zone_id: zone.id,
-          nameservers: zone.name_servers,
-        },
-      });
     }
 
-    const url = await prisma.uRL.findFirst({
-      where: { url: result.data.domain },
-    });
-
-    return redirect(
-      "/dashboard/domain/add?domain=" +
-        result.data.domain +
-        "&zid=" +
-        url!.zone_id +
-        "&progress=nameservers"
-    );
-  }
-
-  if (requestType === "updated_nameservers") {
-    await prisma.uRL.update({
-      where: {
-        url: result.data.domain,
-      },
+    const domain = await prisma.uRL.create({
       data: {
-        progress: Progress.WAITING,
+        donator_id: user!.id,
+        url: result.data.domain!,
+        public: false,
+        connected: false,
+        zone_id: zone!.id,
+        nameservers: zone?.name_servers,
       },
     });
-    return redirect("/");
+
+    return redirect('/dashboard/domain/add?domain=' + domain.url);
   }
+
+  
 }
