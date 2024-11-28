dd5294e667122b1060e2d1f96eeb95a120506829
jay <jay@jeepies.codes>
2024-11-28 11:32:57 +0000

    fix sharenix config being incorrect

--- app/routes/api.sharenix.$id.tsx
+++ app/routes/api.sharenix.$id.tsx
@@ -1,4 +1,5 @@
 import { json, LoaderFunctionArgs } from "@remix-run/node";
+import { M } from "node_modules/vite/dist/node/types.d-aGj9QkWt";
 import { prisma } from "~/services/database.server";
 
 export async function loader({ params }: LoaderFunctionArgs) {
@@ -13,16 +14,19 @@ export async function loader({ params }: LoaderFunctionArgs) {
 
   const config = {
     DefaultImageUploader: "jays.pics",
+    DefaultUrlShortener: "jays.pics",
+    DefaultFileUploader: "jays.pics",
+    ClipboardTime: 5,
     Services: [
-        {
-            Name: "jays.pics",
-            RequestType: "POST",
-            RequestURL: `https://jays.pics/upload?upload_key=${user.upload_key}`,
-            FileFormName: "image",
-            ResponseType: "Text",
-            URL: "$json.url$"
-        }
-    ]
+      {
+        Name: "jays.pics",
+        RequestType: "POST",
+        RequestURL: `https://jays.pics/upload?upload_key=${user.upload_key}`,
+        FileFormName: "image",
+        ResponseType: "Text",
+        URL: "$json.url$",
+      },
+    ],
   };
 
   return new Response(JSON.stringify(config), {
