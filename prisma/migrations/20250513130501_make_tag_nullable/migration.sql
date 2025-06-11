f1a43a03e29aa53b93375f2d90b78f75a887f279
jay <jay@jeepies.codes>
2025-06-11 10:42:30 +0100

    Revert "make tag nullable on image table"
    This reverts commit 0b17c9bea695427b7a0e90c9a048754b0d792d7c.

--- prisma/migrations/20250513130501_make_tag_nullable/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Image" DROP CONSTRAINT "Image_tag_id_fkey";
-
--- AlterTable
-ALTER TABLE "Image" ALTER COLUMN "tag_id" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "Image" ADD CONSTRAINT "Image_tag_id_fkey" FOREIGN KEY ("tag_id") REFERENCES "Tag"("id") ON DELETE SET NULL ON UPDATE CASCADE;
