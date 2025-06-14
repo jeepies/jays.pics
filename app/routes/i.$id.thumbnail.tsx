d98fb3d79641c86cd8a5d76549d892c648f3680c
jay <jay@jeepies.codes>
2025-06-14 19:49:50 +0100

    add size variable to thumbnail route

--- app/routes/i.$id.thumbnail.tsx
+++ app/routes/i.$id.thumbnail.tsx
@@ -4,17 +4,24 @@ import sharp from 'sharp';
 import { prisma } from '~/services/database.server';
 import { get } from '~/services/s3.server';
 
-export async function loader({ params }: LoaderFunctionArgs) {
+export async function loader({ params, request }: LoaderFunctionArgs) {
   const image = await prisma.image.findFirst({ where: { id: params.id } });
   if (!image) return new Response('Not found', { status: 404 });
   const user = await prisma.user.findFirst({ where: { id: image.uploader_id } });
   if (!user) return new Response('Not found', { status: 404 });
 
+
+  const url = new URL(request.url);
+  let size = url.searchParams.get('size');
+  if(!size) size = "256";
+
+  const intSize = parseInt(size);
+
   const data = await get(`${user.id}/${image.id}`);
   const arrayBuffer = await data.arrayBuffer();
   const buffer = Buffer.from(arrayBuffer);
 
-  const resized = await sharp(buffer).resize({ width: 256, height: 256, fit: 'inside' }).toBuffer();
+  const resized = await sharp(buffer).resize({ width: intSize, height: intSize, fit: 'inside' }).toBuffer();
 
   return new Response(resized, {
     headers: {
