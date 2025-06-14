f9c64e27e460a65871eca7456b8aa36c43bdf7dc
jay <jay@jeepies.codes>
2025-06-14 16:39:15 +0100

    fix sidebar padding

--- app/components/ui/sidebar.tsx
+++ app/components/ui/sidebar.tsx
@@ -35,21 +35,13 @@ interface SidebarProps {
 
 export function Sidebar({ className, user, version, onLinkClick }: Readonly<SidebarProps>) {
   const [showTray, setShowTray] = useState(false);
-  const [notifications, setNotifications] = useState(
-    user.notifications ?? []
-  );
+  const [notifications, setNotifications] = useState(user.notifications ?? []);
 
-  const removeNotification = (id: string) =>
-    setNotifications((prev) => prev.filter((n) => n.id !== id));
+  const removeNotification = (id: string) => setNotifications((prev) => prev.filter((n) => n.id !== id));
 
   return (
     <div className={cn('pb-12 w-64 relative', className)}>
-      {showTray && (
-        <NotificationTray
-          notifications={notifications}
-          onRemove={removeNotification}
-        />
-      )}
+      {showTray && <NotificationTray notifications={notifications} onRemove={removeNotification} />}
       <div className="space-y-4 py-4">
         <div className="px-3 py-2">
           <h2 className="mb-2 px-4 text-lg font-semibold tracking-tight text-gray-900 dark:text-gray-100">
@@ -57,47 +49,80 @@ export function Sidebar({ className, user, version, onLinkClick }: Readonly<Side
             {!user.notifications || user.notifications.length === 0 ? (
               <Bell className="float-right w-4 m-1 cursor-pointer" onClick={() => setShowTray(!showTray)} />
             ) : (
-              <BellDotIcon className="float-right w-4 m-1 hover:text-accent cursor-pointer" onClick={() => setShowTray(!showTray)} />
+              <BellDotIcon
+                className="float-right w-4 m-1 hover:text-accent cursor-pointer"
+                onClick={() => setShowTray(!showTray)}
+              />
             )}
           </h2>
           <Separator className="my-4" />
           <div className="space-y-1">
-            <Button onClick={onLinkClick} variant="ghost" className="w-full justify-start text-gray-900 dark:text-gray-100">
-              <Link to="/dashboard/index">
-                <Home className="mr-2 h-4 w-4" />
+            <Button
+              onClick={onLinkClick}
+              asChild
+              variant="ghost"
+              className="w-full justify-start text-gray-900 dark:text-gray-100"
+            >
+              <Link to="/dashboard/index" className="flex items-center gap-2">
+                <Home className="h-4 w-4" />
                 Dashboard
               </Link>
             </Button>
-            <Button onClick={onLinkClick} variant="ghost" className="w-full justify-start text-gray-900 dark:text-gray-100">
-              <Link to="/dashboard/images">
-                <Image className="mr-2 h-4 w-4" />
+            <Button
+              onClick={onLinkClick}
+              asChild
+              variant="ghost"
+              className="w-full justify-start text-gray-900 dark:text-gray-100"
+            >
+              <Link to="/dashboard/images" className="flex items-center gap-2">
+                <Image className="h-4 w-4" />
                 Images
-                {/* <span className="ml-auto bg-secondary text-secondary-foreground rounded-md px-2 py-0.5 text-xs">
-                  {user.images.length}
-                </span> */}
+                <span className="ml-auto bg-secondary text-secondary-foreground rounded-md px-2 py-0.5 text-xs">
+                  1.1K
+                </span>
               </Link>
             </Button>
-            <Button onClick={onLinkClick} variant="ghost" className="w-full justify-start text-gray-900 dark:text-gray-100">
-              <Link to="/dashboard/referrals">
-                <Link2 className="mr-2 h-4 w-4" />
+            <Button
+              onClick={onLinkClick}
+              asChild
+              variant="ghost"
+              className="w-full justify-start text-gray-900 dark:text-gray-100"
+            >
+              <Link to="/dashboard/referrals" className="flex items-center gap-2">
+                <Link2 className="h-4 w-4" />
                 Referrals
               </Link>
             </Button>
-            <Button onClick={onLinkClick} variant="ghost" className="w-full justify-start text-gray-900 dark:text-gray-100">
-              <Link to="/dashboard/upload-settings">
-                <ImageIcon className="mr-2 h-4 w-4" />
+            <Button
+              onClick={onLinkClick}
+              asChild
+              variant="ghost"
+              className="w-full justify-start text-gray-900 dark:text-gray-100"
+            >
+              <Link to="/dashboard/upload-settings" className="flex items-center gap-2">
+                <ImageIcon className="h-4 w-4" />
                 Upload Config
               </Link>
             </Button>
-            <Button onClick={onLinkClick} variant="ghost" className="w-full justify-start text-gray-900 dark:text-gray-100">
-              <Link to="/dashboard/domains">
-                <Globe2 className="mr-2 h-4 w-4" />
+            <Button
+              onClick={onLinkClick}
+              asChild
+              variant="ghost"
+              className="w-full justify-start text-gray-900 dark:text-gray-100"
+            >
+              <Link to="/dashboard/domains" className="flex items-center gap-2">
+                <Globe2 className="h-4 w-4" />
                 Domains
               </Link>
             </Button>
-            <Button onClick={onLinkClick} variant="ghost" className="w-full justify-start text-gray-900 dark:text-gray-100">
-              <Link to="/dashboard/help">
-                <FileQuestion className="mr-2 h-4 w-4" />
+            <Button
+              onClick={onLinkClick}
+              asChild
+              variant="ghost"
+              className="w-full justify-start text-gray-900 dark:text-gray-100"
+            >
+              <Link to="/dashboard/help" className="flex items-center gap-2">
+                <FileQuestion className="h-4 w-4" />
                 Help
               </Link>
             </Button>
@@ -106,25 +131,40 @@ export function Sidebar({ className, user, version, onLinkClick }: Readonly<Side
       </div>
       <div className="absolute bottom-4 left-0 right-0 px-3">
         <div className="space-y-1">
-          <Button onClick={onLinkClick} variant="ghost" className="w-full justify-start text-gray-900 dark:text-gray-100">
-            <Link to="/dashboard/settings">
-              <Cog className="mr-2 h-4 w-4" />
+          <Button
+            onClick={onLinkClick}
+            asChild
+            variant="ghost"
+            className="w-full justify-start text-gray-900 dark:text-gray-100"
+          >
+            <Link to="/dashboard/settings" className="flex items-center gap-2">
+              <Cog className="h-4 w-4" />
               Settings
             </Link>
           </Button>
-          <Button onClick={onLinkClick} variant="ghost" className="w-full justify-start text-gray-900 dark:text-gray-100">
-            <Link to="/profile/me">
-              <User className="mr-2 h-4 w-4" />
+          <Button
+            onClick={onLinkClick}
+            asChild
+            variant="ghost"
+            className="w-full justify-start text-gray-900 dark:text-gray-100"
+          >
+            <Link to="/profile/me" className="flex items-center gap-2">
+              <User className="h-4 w-4" />
               {user.username}
             </Link>
           </Button>
-          <Button onClick={onLinkClick} variant="ghost" className="w-full justify-start text-gray-900 dark:text-gray-100">
+          <Button asChild variant="ghost" className="w-full justify-start text-gray-900 dark:text-gray-100">
             <ThemeToggle />
           </Button>
           {user.is_admin ? (
-            <Button onClick={onLinkClick} variant="ghost" className="w-full justify-start text-gray-900 dark:text-gray-100">
-              <Link to="/admin/index">
-                <Shield className="mr-2 h-4 w-4" />
+            <Button
+              onClick={onLinkClick}
+              asChild
+              variant="ghost"
+              className="w-full justify-start text-gray-900 dark:text-gray-100"
+            >
+              <Link to="/admin/index" className="flex items-center gap-2">
+                <Shield className="h-4 w-4" />
                 Admin Dashboard
               </Link>
             </Button>
@@ -132,12 +172,13 @@ export function Sidebar({ className, user, version, onLinkClick }: Readonly<Side
             <></>
           )}
           <Button
+            onClick={onLinkClick}
             asChild
             variant="ghost"
             className="w-full justify-start hover:bg-red-700 hover:text-white text-gray-900 dark:text-gray-100"
           >
-            <Link to="/logout">
-              <LogOut className="mr-2 h-4 w-4" />
+            <Link to="/logout" className="flex items-center gap-2">
+              <LogOut className="h-4 w-4" />
               Log out
             </Link>
           </Button>
