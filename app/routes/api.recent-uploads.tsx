ca7c746acc203a83f6a6d8c693067e0be8e8a491
jay <jay@jeepies.codes>
2025-06-15 10:58:49 +0100

    add upload token to recent-uploads route

--- app/routes/api.recent-uploads.tsx
+++ app/routes/api.recent-uploads.tsx
@@ -1,17 +1,26 @@
-import { ActionFunctionArgs, json, LoaderFunctionArgs } from '@remix-run/node';
+import { json, LoaderFunctionArgs } from '@remix-run/node';
 
 import { prisma } from '~/services/database.server';
 import { getSession, getUserBySession } from '~/services/session.server';
 
-export async function action({ request }: ActionFunctionArgs) {
+export async function action() {
   return json({ error: 'POST not supported' });
 }
 
 export async function loader({ request }: LoaderFunctionArgs) {
-  const session = await getSession(request.headers.get('Cookie'));
-  if (!session.has('userID')) return json({ error: 'Not authorized' });
-  const user = await getUserBySession(session);
-  if(!user) return json({ error: 'Not authorized' });
+  const url = new URL(request.url);
+  const uploadKey = url.searchParams.get('upload_key');
+  let user = null;
+
+  if (uploadKey) {
+    user = await prisma.user.findFirst({ where: { upload_key: uploadKey } });
+  } else {
+    const session = await getSession(request.headers.get('Cookie'));
+    if (!session.has('userID')) return json({ error: 'Not authorized' });
+    user = await getUserBySession(session);
+  }
+
+  if (!user) return json({ error: 'Not authorized' });
 
   const images = await prisma.image.findMany({
     where: { uploader_id: user.id },
