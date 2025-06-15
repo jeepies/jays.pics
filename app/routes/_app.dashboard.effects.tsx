4d25f646bc4f922d3549c01114571ad91711544c
jay <jay@jeepies.codes>
2025-06-15 09:39:57 +0100

    add new routes

---
+++ app/routes/_app.dashboard.effects.tsx
@@ -0,0 +1,14 @@
+import { Card, CardContent, CardHeader, CardTitle } from '~/components/ui/card';
+
+export default function Effects() {
+  return (
+    <div className="container mx-auto px-4 py-8">
+      <Card>
+        <CardHeader>
+          <CardTitle>Effects</CardTitle>
+        </CardHeader>
+        <CardContent>TODO</CardContent>
+      </Card>
+    </div>
+  );
+}
