0253b36e74a54bfe650160637640308986d34691
jay <jay@jeepies.codes>
2025-06-15 15:18:13 +0100

    update oembed

--- app/routes/i.$id.oembed[.json].tsx
+++ app/routes/i.$id.oembed[.json].tsx
@@ -1,25 +1,25 @@
-import { json, LoaderFunctionArgs, MetaFunction } from "@remix-run/node";
-import prettyBytes from "pretty-bytes";
+import { json, LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
+import prettyBytes from 'pretty-bytes';
 
-import { templateReplacer } from "~/lib/utils";
-import { prisma } from "~/services/database.server";
+import { templateReplacer } from '~/lib/utils';
+import { prisma } from '~/services/database.server';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "oEmbed | jays.pics" },
-    { name: "description", content: "Administration Dashboard" },
+    { title: 'oEmbed | jays.pics' },
+    { name: 'description', content: 'Administration Dashboard' },
     {
-      name: "theme-color",
-      content: "#e05cd9",
+      name: 'theme-color',
+      content: '#e05cd9',
     },
   ];
 };
 
 export async function loader({ params }: LoaderFunctionArgs) {
   const image = await prisma.image.findFirst({ where: { id: params.id } });
-  if (!image) return json({ success: false, message: "Image does not exist" });
+  if (!image) return json({ success: false, message: 'Image does not exist' });
 
-  const uploader = await prisma.user.findFirst({
+  const uploaderData = await prisma.user.findFirst({
     where: { id: image!.uploader_id },
     select: {
       username: true,
@@ -30,29 +30,35 @@ export async function loader({ params }: LoaderFunctionArgs) {
     },
   });
 
-  const dictionary = {
-    "image.name": image?.display_name,
-    "image.size_bytes": image?.size,
-    "image.size": prettyBytes(image!.size),
-    "image.created_at": image?.created_at,
+  const uploader =
+    uploaderData ?
+      {
+        ...uploaderData,
+        space_used: Number(uploaderData.space_used),
+        max_space: Number(uploaderData.max_space),
+      }
+    : null;
 
-    "uploader.name": uploader?.username,
-    "uploader.storage_used_bytes": uploader?.space_used,
-    "uploader.storage_used": prettyBytes(uploader!.space_used),
-    "uploader.total_storage_bytes": uploader?.max_space,
-    "uploader.total_storage": prettyBytes(uploader!.max_space),
+  const dictionary = {
+    'image.name': image?.display_name,
+    'image.size_bytes': image?.size,
+    'image.size': prettyBytes(image!.size),
+    'image.created_at': image?.created_at,
+
+    'uploader.name': uploader?.username,
+    'uploader.storage_used_bytes': uploader?.space_used,
+    'uploader.storage_used': prettyBytes(uploader!.space_used),
+    'uploader.total_storage_bytes': uploader?.max_space,
+    'uploader.total_storage': prettyBytes(uploader!.max_space),
   };
 
-  const author = templateReplacer(
-    uploader?.upload_preferences?.embed_author ?? "",
-    dictionary,
-  );
+  const author = templateReplacer(uploader?.upload_preferences?.embed_author ?? '', dictionary);
 
   return json({
     author_name: author,
     author_url: `https://jays.pics/profile/${uploader?.id}`,
-    provider_name: "Hosted with 🩵 at jays.pics",
-    provider_url: "https://jays.pics",
-    type: "photo",
+    provider_name: 'Hosted with 🩵 at jays.pics',
+    provider_url: 'https://jays.pics',
+    type: 'photo',
   });
 }
