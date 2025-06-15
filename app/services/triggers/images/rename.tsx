f0f2622fe51ab0de8170bebad3a6759458abf97d
jay <jay@jeepies.codes>
2025-06-15 13:10:51 +0100

    update triggers

---
+++ app/services/triggers/images/rename.tsx
@@ -0,0 +1,20 @@
+import { prisma } from '~/services/database.server';
+import { Input } from '~/components/ui/input';
+
+export default async function rename({ imageId, data }: { imageId: string; data: any }) {
+  if (!data.name) return;
+  await prisma.image.update({
+    where: { id: imageId },
+    data: { display_name: data.name },
+  });
+}
+
+export function Component({ data, update }: { data: any; update: (d: any) => void }) {
+  return (
+    <Input
+      placeholder="New Name"
+      value={data.name || ''}
+      onChange={(e) => update({ name: e.target.value })}
+    />
+  );
+}
